b9f0ea01666dfeec084345a8639880d772ff2fd9
guitarbien <guitarbien@gmail.com>
2015-08-15 10:47:25 -0400

    set what filed can be filled in Article

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+	// 定義當使用 __construct($data) 或 create($data) 時
+	// 可以被修改的欄位，進而保護其他欄位不被修改
+	protected $fillable = ['title', 'body'];
 }
