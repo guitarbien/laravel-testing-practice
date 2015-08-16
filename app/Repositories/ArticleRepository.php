c9556cedcd60a80c9004c434eabf2550df30944d
guitarbien <guitarbien@gmail.com>
2015-08-16 00:54:02 -0400

    using repostory to package model

---
+++ app/Repositories/ArticleRepository.php
@@ -0,0 +1,16 @@
+namespace App\Repositories;
+
+use App\Article;
+
+class ArticleRepository
+{
+    public function latest10()
+    {
+        return Article::query()->orderBy('id', 'desc')->limit(10)->get();
+    }
+
+    public function create(array $attributes)
+    {
+        return Article::create($attributes);
+    }
+}
